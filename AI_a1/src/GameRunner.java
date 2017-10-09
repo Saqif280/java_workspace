0cf64a9ff24e0bf971fbe7dbc4fdc5bae8bab2b8
Saqif Badruddin <saqif280@gmail.com>
2017-10-10 00:02:21 +0100

    setup runner

---
+++ AI_a1/src/GameRunner.java
@@ -0,0 +1,9 @@
+
+public class GameRunner {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeConstructor game = new TreeConstructor(10,10,10);
+		System.out.print(game.getRoot().getE());
+	}
+}
