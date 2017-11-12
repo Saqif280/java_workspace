ad0d28b4b405824cca8c1bdb9158ab4ef76e61a9
Saqif Badruddin <saqif280@gmail.com>
2017-11-12 21:37:07 +0000

    human player input works

--- AI_a2/src/GameBoard.java
+++ AI_a2/src/GameBoard.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  * Name: Saqif Badruddin
@@ -22,7 +23,257 @@ public class GameBoard {
     private int[] dcCell2;
     private Character currPlayer = PLAYER1;
     
-    // constructor
+    // make gameboard from prompts
+    public GameBoard() {
+        // welcome message  
+        System.out.println("Welcome to Much-modified Connect Four!\n");
+        
+        // board arguments
+        int numColumns = 0;
+        int[] c;
+        int[] dc1 = new int[2];
+        int[] dc2 = new int[2];
+        
+        Scanner sc = new Scanner(System.in);
+        
+        // ask for num columns
+        while(numColumns == 0) {
+            System.out.println("How many columns? (6-11)");
+            // if input is integer
+            if(sc.hasNextInt()) {
+                // get integer
+                numColumns = sc.nextInt();
+                // if not within bounds, reset num columns value
+                if(numColumns < 6 || numColumns > 11) {
+                    numColumns = 0;
+                }
+            } else {
+                sc.next();
+            }
+        }
+        c = new int[numColumns];
+        
+        // ask for column lengths
+        for(int i=0;i<numColumns;i++) {
+            c[i] = -1;
+            while(c[i] == -1) {
+                System.out.println("How long is column "+(i+1)+"?");
+                // if input is integer
+                if(sc.hasNextInt()) {
+                    // get integer
+                    int length = sc.nextInt();
+                    // if within bounds, set columns value
+                    if(length > 0) {
+                        c[i] = length;
+                    }
+                } else {
+                    sc.next();
+                }
+            }
+        }
+        
+        // ask for dont-count cell positions
+        // cell1
+        int tempCol=-1;
+        int tempRow=-1;
+        System.out.println("For first don't-count cell:");
+        while(tempCol==-1) {
+            System.out.println("Column number? (1-"+numColumns+")");
+            // if input is integer
+            if(sc.hasNextInt()) {
+                // get integer
+                tempCol = sc.nextInt()-1;
+                // if not within bounds, reset columns value
+                if(tempCol < 0 || tempCol >= numColumns) {
+                    tempCol = -1;
+                }
+            } else {
+                sc.next();
+            }
+        }
+        while(tempRow==-1) {
+            System.out.println("Column number? (1-"+c[tempCol]+")");
+            // if input is integer
+            if(sc.hasNextInt()) {
+                // get integer
+                tempRow = sc.nextInt()-1;
+                // if not within bounds, reset columns value
+                if(tempRow < 0 || tempRow >= c[tempCol]) {
+                    tempRow = -1;
+                }
+            } else {
+                sc.next();
+            }
+        }
+        dc1 = new int[] {tempCol, tempRow};
+        // cell2
+        tempCol=-1;
+        tempRow=-1;
+        System.out.println("For second don't-count cell:");
+        while(tempCol==-1) {
+            System.out.println("Column number? (1-"+numColumns+")");
+            // if input is integer
+            if(sc.hasNextInt()) {
+                // get integer
+                tempCol = sc.nextInt()-1;
+                // if not within bounds, reset columns value
+                if(tempCol < 0 || tempCol >= numColumns) {
+                    tempCol = -1;
+                }
+            } else {
+                sc.next();
+            }
+        }
+        while(tempRow==-1) {
+            System.out.println("Column number? (1-"+c[tempCol]+")");
+            // if input is integer
+            if(sc.hasNextInt()) {
+                // get integer
+                tempRow = sc.nextInt()-1;
+                // if not within bounds, reset columns value
+                if(tempRow < 0 || tempRow >= c[tempCol]) {
+                    tempRow = -1;
+                }
+            } else {
+                sc.next();
+            }
+        }
+        dc2 = new int[] {tempCol, tempRow};
+        
+        // finished collecting values for game board
+        System.out.println("\nThank you! Now generating game board ...");
+        dcCell1 = dc1;
+        dcCell2 = dc2;
+        
+        // initialize gameboard size
+        int cols = c.length;
+        int rows = 0;
+        for(int i=0;i<cols;i++) {
+            rows = Math.max(rows, c[i]);
+        }
+        columns = new Character[cols][rows];
+        
+        // populate gameboard
+        for(int col=0;col<columns.length;col++) {
+            for(int row=0;row<columns[col].length;row++) {
+                if (col==dcCell1[0] && row==dcCell1[1]
+                        || col==dcCell2[0] && row==dcCell2[1]) {
+                    // dont count cells
+                    columns[col][row] = DC;
+                } else if(row < c[col]) {
+                    // empty cells
+                    columns[col][row] = EMPTY;
+                } else {
+                    // unplayable cells
+                    columns[col][row] = UNPLAYABLE;
+                }
+                
+            }
+        }
+        printBoard();
+        
+        // asks whos playing
+        System.out.println("\nWho will be playing?");
+        int players = 0;
+        while(players==0) {
+            System.out.println("(1) computer(X) vs computer(O)");
+            System.out.println("(2) human(X) vs computer(O)");
+            if(sc.hasNextInt()) {
+                players = sc.nextInt();
+                if(players != 1 && players != 2) {
+                    players = 0;
+                }
+            } else {
+                sc.next();
+            }
+        }
+        players--;
+        // ask for who plays first
+        int tempCurrPlayer = 0;
+        while(tempCurrPlayer==0) {
+            System.out.println("Who moves first? (1)"+PLAYER1+" or (2)"+PLAYER2);
+            if(sc.hasNextInt()) {
+                tempCurrPlayer = sc.nextInt();
+                if(tempCurrPlayer != 1 && tempCurrPlayer != 2) {
+                    tempCurrPlayer = 0;
+                }
+            } else {
+                sc.next();
+            }
+        }
+        currPlayer=(tempCurrPlayer==1)?PLAYER1:PLAYER2;
+        
+        // ask for computer difficulty
+        int comp1diff = -1;
+        int comp2diff = -1;
+        while(comp1diff==-1) {
+            System.out.println("What is one computer player's difficulty? (1-2500)");
+            if(sc.hasNextInt()) {
+                comp1diff = sc.nextInt();
+                if(comp1diff < 1 || comp1diff > 2500) {
+                    comp1diff = -1;
+                }
+            } else {
+                sc.next();
+            }
+        }
+        if(players==0) {
+            while(comp2diff==-1) {
+                System.out.println("What is two computer player's difficulty? (1-2500)");
+                if(sc.hasNextInt()) {
+                    comp2diff = sc.nextInt();
+                    if(comp2diff < 1 || comp2diff > 2500) {
+                        comp2diff = -1;
+                    }
+                } else {
+                    sc.next();
+                }
+            }
+        }  
+        System.out.println("\nThank you! Now we will begin the game ...");
+        
+        // if human participant
+        if(players==1) {
+            // ask to make move until game ends
+            while(!(boolean)checkGameCompletion().get(0)) {
+                System.out.println();
+                // if humans turn
+                if(currPlayer==PLAYER1) {
+                    // ask for move col
+                    int col = -1;
+                    while(col==-1) {
+                        System.out.println("Your move. What column? "
+                                + "(1-"+columns.length+")");
+                        if(sc.hasNextInt()) {
+                            col = sc.nextInt();
+                            if(!makeMove(col)) {
+                                col = -1;
+                            }
+                        } else {
+                            sc.next();
+                        }
+                    }
+                } else {
+                    // make comp move
+                    int col = 1+(int)(Math.random() * columns.length);
+                    makeMove(col);
+                    System.out.println("Computer move");
+                }
+                printBoard();
+                System.out.println((String)checkGameCompletion().get(1));
+            }
+        } else {
+            // play comp game
+            System.out.println("Not implemented yet.");
+        }
+            
+        
+        // close scanner
+        sc.close();
+        System.out.println("Done.");
+    }
+    
+    // make gameboard with specs
     public GameBoard(int[] c, int[] dc1, int[] dc2) {
         dcCell1 = dc1;
         dcCell2 = dc2;
@@ -55,10 +306,10 @@ public class GameBoard {
     }
     
     // make a move on the board
-    public boolean makeMove(int c) {
+    private boolean makeMove(int c) {
         c--;
         // if out of bounds
-        if(c >= columns.length) {
+        if(c >= columns.length || c<0) {
             return false;
         }
         // if there is a playable spot
@@ -78,24 +329,8 @@ public class GameBoard {
         return false;
     }
     
-    // print board
-    public void printBoard() {
-        System.out.println("\nGame Board:");
-        for(int r=columns[0].length-1;r>=0;r--) {
-            System.out.print("R"+(r+1)+" ");
-            for(int c=0;c<columns.length;c++) {
-                System.out.print("["+columns[c][r]+"]");
-            }
-            System.out.println();
-        }
-        System.out.print("    ");
-        for(int c=0;c<columns.length;c++) {
-            System.out.print("C"+(c+1)+" ");
-        }
-        System.out.print("\n");
-    }
-    
-    public ArrayList<Object> checkGameCompletion() {
+    // check if game is done
+    private ArrayList<Object> checkGameCompletion() {
         ArrayList<Object> retArray = new ArrayList<Object>();;
         int emptyPositionsCount = 0;
         // if four in a row, return true
@@ -256,6 +491,23 @@ public class GameBoard {
         retArray.add(false);
         retArray.add("No one has won yet.");
         return retArray;
+    }
+    
+    // print board
+    private void printBoard() {
+        System.out.println("\nGame Board:");
+        for(int r=columns[0].length-1;r>=0;r--) {
+            System.out.print("R"+(r+1)+" ");
+            for(int c=0;c<columns.length;c++) {
+                System.out.print("["+columns[c][r]+"]");
+            }
+            System.out.println();
+        }
+        System.out.print("    ");
+        for(int c=0;c<columns.length;c++) {
+            System.out.print("C"+(c+1)+" ");
+        }
+        System.out.print("\n\n");
     }
     
     
